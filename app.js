8711286625b6196f0b8243a77e41434cdd73a68f
Romaric Philogene <evoxmusic@gmail.com>
2020-02-21 11:35:12 +0100

    update: :)

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({"hello": "world"});
+    res.send({"prime": "number"});
 });
 
 // Listen to the App Engine-specified port, or 8080 otherwise
